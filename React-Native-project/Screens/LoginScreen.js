e30cf7ed244aa9e76e33cdfb52d53afd26d62a4d
seacat271 <seacat271083@gmail.com>
2023-01-04 17:03:48 +0200

    add login page

--- React-Native-project/Screens/LoginScreen.js
+++ React-Native-project/Screens/LoginScreen.js
@@ -1,3 +1,45 @@
+import { View, Text, StyleSheet, TextInput } from "react-native";
 export const LoginScreen = () => {
-  return <></>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Войти</Text>
+      <TextInput
+        style={styles.input}
+        placeholderTextColor={"#BDBDBD"}
+        placeholder={"Адрес електронной почты"}
+      />
+      <TextInput
+        style={styles.input}
+        placeholderTextColor={"#BDBDBD"}
+        placeholder={"Пароль"}
+        textContentType={"password"}
+      />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    height: 549,
+    backgroundColor: "#ffffff",
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+    padding: 16,
+  },
+  title: {
+    textAlign: "center",
+    marginBottom: 32,
+    fontSize: 30,
+  },
+  input: {
+    height: 50,
+    backgroundColor: "#F6F6F6",
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    marginBottom: 16,
+    color: "#212121",
+    fontSize: 16,
+  },
+});
