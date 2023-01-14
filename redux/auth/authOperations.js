365858352288b07d9608ea3621b736056f829c9b
seacat271 <seacat271083@gmail.com>
2023-01-15 00:09:21 +0200

    add updateProfile

--- redux/auth/authOperations.js
+++ redux/auth/authOperations.js
@@ -2,18 +2,18 @@ import auth from "../../firebase/config";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  updateProfile,
 } from "firebase/auth";
 import { Alert } from "react-native";
+import { authSlice } from "./authSlice";
 const authSignInUser =
   ({ email, password }) =>
   async (dispatch, getState) => {
     try {
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
+      const { user } = await signInWithEmailAndPassword(auth, email, password);
+      dispatch(
+        authSlice.actions.updateUserProfile({ userId: uid, login: displayName })
       );
-      console.log(userCredential);
     } catch (error) {
       console.log(error);
       Alert.alert(error.message);
@@ -23,17 +23,20 @@ const authSignUpUser =
   ({ email, password, login }) =>
   async (dispatch, getState) => {
     try {
-      const auth = getAuth(app);
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
+      await createUserWithEmailAndPassword(auth, email, password);
+      const user = auth.currentUser;
+      await updateProfile(auth.currentUser, {
+        displayName: login,
+      });
+      const { uid, displayName } = auth.currentUser;
+      dispatch(
+        authSlice.actions.updateUserProfile({ userId: uid, login: displayName })
       );
-      console.log(userCredential);
     } catch (error) {
       console.log(error);
       console.log(error.message);
     }
   };
 const authSignOutUser = () => async (dispatch, getState) => {};
+
 export { authSignInUser, authSignUpUser, authSignOutUser };
--- redux/auth/authSlice.js
+++ redux/auth/authSlice.js
@@ -3,7 +3,18 @@ export const authSlice = createSlice({
   name: "auth",
   initialState: {
     userId: null,
-    name: null,
+    login: null,
+    stateChange: false,
   },
-  reducers: {},
+  reducers: {
+    updateUserProfile: (state, { payload }) => ({
+      ...state,
+      userId: payload.userId,
+      login: payload.login,
+    }),
+    authStateChange: (state, { payload }) => ({
+      ...state,
+      stateChange: payload.stateChange,
+    }),
+  },
 });
